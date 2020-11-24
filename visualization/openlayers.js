6bfadee2c9c4bb2e2a8672360f725bea284a4362
jurgendn <nguyentiendung7299@gmail.com>
2020-11-24 18:41:47 +0700

    add visualization by map

---
+++ visualization/openlayers.js
@@ -0,0 +1,108 @@
+epsg4326 = new OpenLayers.Projection("EPSG:4326");
+
+map = new OpenLayers.Map({
+  div: "mapdiv",
+  displayProjection: epsg4326, // With this setting, lat and lon are displayed correctly in MousePosition and permanent anchor
+});
+
+//   map = new OpenLayers.Map("mapdiv");
+map.addLayer(new OpenLayers.Layer.OSM());
+map.addLayer(
+  new OpenLayers.Layer.OSM(
+    "Wikimedia",
+    ["https://maps.wikimedia.org/osm-intl/${z}/${x}/${y}.png"],
+    {
+      attribution:
+        "&copy; <a href='http://www.openstreetmap.org/'>OpenStreetMap</a> and contributors, under an <a href='http://www.openstreetmap.org/copyright' title='ODbL'>open license</a>. <a href='https://www.mediawiki.org/wiki/Maps'>Wikimedia's new style (beta)</a>",
+      tileOptions: { crossOriginKeyword: null },
+    }
+  )
+);
+// See https://wiki.openstreetmap.org/wiki/Tile_servers for other OSM-based layers
+
+map.addControls([
+  new OpenLayers.Control.MousePosition(),
+  new OpenLayers.Control.ScaleLine(),
+  new OpenLayers.Control.LayerSwitcher(),
+  new OpenLayers.Control.Permalink({ anchor: true }),
+]);
+
+projectTo = map.getProjectionObject(); //The map projection (Spherical Mercator)
+var lonLat = new OpenLayers.LonLat(108.212, 16.068).transform(
+  epsg4326,
+  projectTo
+);
+var zoom = 7;
+if (!map.getCenter()) {
+  map.setCenter(lonLat, zoom);
+}
+
+let data;
+
+$.ajax({
+  async: false,
+  global: false,
+  url: "data2.json",
+  dataType: "json",
+  success: function (dt) {
+    data = dt;
+  },
+});
+
+let patient = data.nodes;
+
+// Put your point-definitions here
+var markers = [];
+
+patient.forEach((node) => {
+  markers.push([node.x, node.y, node.name]);
+});
+
+var colorList = ["red", "blue", "yellow"];
+var layerName = [markers[0][2]];
+var styleArray = [
+  new OpenLayers.StyleMap({
+    pointRadius: 6,
+    fillColor: colorList[0],
+    fillOpacity: 0.5,
+  }),
+];
+var vectorLayer = [
+  new OpenLayers.Layer.Vector(layerName[0], { styleMap: styleArray[0] }),
+]; // First element defines first Layer
+
+var j = 0;
+for (var i = 1; i < markers.length; i++) {
+  if (!layerName.includes(markers[i][2])) {
+    j++;
+    layerName.push(markers[i][2]); // If new layer name found it is created
+    styleArray.push(
+      new OpenLayers.StyleMap({
+        pointRadius: 6,
+        fillColor: colorList[j % colorList.length],
+        fillOpacity: 1,
+      })
+    );
+    vectorLayer.push(
+      new OpenLayers.Layer.Vector(layerName[j], {
+        styleMap: styleArray[j],
+      })
+    );
+  }
+}
+
+//Loop through the markers array
+for (var i = 0; i < markers.length; i++) {
+  var lon = markers[i][0];
+  var lat = markers[i][1];
+  var feature = new OpenLayers.Feature.Vector(
+    new OpenLayers.Geometry.Point(lon, lat).transform(epsg4326, projectTo),
+    { description: "marker number " + i }
+    // see http://dev.openlayers.org/docs/files/OpenLayers/Feature/Vector-js.html#OpenLayers.Feature.Vector.Constants for more options
+  );
+  vectorLayer[layerName.indexOf(markers[i][2])].addFeatures(feature);
+}
+
+for (var i = 0; i < layerName.length; i++) {
+  map.addLayer(vectorLayer[i]);
+}
